97aea095de39aebf225934b64e1af5b6400d3fe3
Alcatraz1221 <111422710+Gonfrx@users.noreply.github.com>
2023-12-22 06:39:23 -0800

    Added solution for Longest Consecutive Sequence

--- week 4/longest-consecutive-sequence.py
+++ week 4/longest-consecutive-sequence.py
@@ -1,22 +1,20 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        nums.sort()
-        mx = 0
-        curr =1
-        for i in range(len(nums)-1): 
-            if nums[i+1] == (nums[i]+1): 
+        s = set(nums)
+        start = []
+        mx  = 0
+        curr = 0
+        for i in range(len(nums)):
+            if nums[i]-1 not in s: 
+                start.append(nums[i])
+        for val in start: 
+            curr = 0
+            v = val 
+            while(v in s):
+                v+=1
                 curr += 1
-                mx = max(curr, mx)
-            elif nums[i+1] == nums[i]:
-                curr += 0
-                mx = max(curr, mx)
-            else: 
-                mx = max(curr, mx)
-                curr = 1
-        if len(nums) == 1:
-            return 1
+            mx = max(curr, mx)
         return mx
-
 
         
         
