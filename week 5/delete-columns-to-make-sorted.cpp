6e5f6854570faac442b90a9ab5183270d34726b9
Alcatraz1221 <111422710+Gonfrx@users.noreply.github.com>
2023-12-26 04:45:48 -0800

    Added solution for Delete Columns to Make Sorted

---
+++ week 5/delete-columns-to-make-sorted.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+       int counter = 0; 
+       string f = ""; 
+       for(int i = 0; i < strs[0].size();i++){
+           for(int j = 1; j < strs.size();j++){
+               if(strs[j][i] < strs[j-1][i]){
+                   counter++; break; 
+               }
+           }
+       }
+       return counter; 
+
+    }
+};
